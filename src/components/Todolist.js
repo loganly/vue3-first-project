33eb5f7764ea07d12b8c3edc69fd8af891a6fafd
LiYang <logan_ly@163.com>
2022-02-22 16:22:27 +0800

    composition api / script setup

---
+++ src/components/Todolist.js
@@ -0,0 +1,39 @@
+import { ref, computed } from 'vue';
+
+export function useTodos() {
+  // 动态style改变颜色
+  let color = ref(Math.random() > 0.5 ? 'blue' : 'red');
+
+  let title = ref('');
+  let todos = ref([{ title: '学习Vue', done: false }]);
+
+  function addTodo() {
+    todos.value.push({
+      title: title.value,
+      done: false,
+    });
+    title.value = '';
+  }
+
+  function clear() {
+    todos.value = todos.value.filter((v) => !v.done);
+  }
+
+  let active = computed(() => {
+    return todos.value.filter((v) => !v.done).length;
+  });
+
+  let all = computed(() => todos.value.length);
+
+  let allDone = computed({
+    get: function () {
+      return active.value === 0;
+    },
+    set: function (value) {
+      todos.value.forEach((todo) => {
+        todo.done = value;
+      });
+    },
+  });
+  return { color, title, todos, addTodo, clear, active, all, allDone };
+}
