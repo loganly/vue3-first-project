0f5b87b34478bc7ca69dd8a95e2d6d1eb04c880c
LiYang <logan_ly@outlook.com>
2022-03-02 14:10:36 +0800

    rate组件

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,8 +1,16 @@
 <template>
   <h1>这是首页</h1>
   <Todolist />
+  <Rate v-model="score" @update-rate="update" />
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import Todolist from '../components/Todolist.vue';
+import Rate from '../components/Rate.vue';
+
+let score = ref(3.5);
+function update(num) {
+  score.value = num;
+}
 </script>
