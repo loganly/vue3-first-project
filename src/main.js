f8db6077a7547942ee35dd290003c4101bdde4af
LiYang <logan_ly@outlook.com>
2022-03-02 15:12:22 +0800

    Vuex

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import state from './state';
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(state).use(router).mount('#app');
